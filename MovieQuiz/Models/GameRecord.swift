7d46dfe2583c6735a2c25f354dce48e19f59a059
Fokusnik909 <Fokusnik90999@gmail.com>
2023-12-22 17:37:10 +0500

    fix bug

---
+++ MovieQuiz/Models/GameRecord.swift
@@ -0,0 +1,8 @@
+//
+//  GameRecord.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 22.12.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 20.12.2023.
+//
+
+import Foundation
