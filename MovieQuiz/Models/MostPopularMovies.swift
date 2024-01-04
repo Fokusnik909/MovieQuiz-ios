fadedf1f537f912ee4cb038aa56a5082da9d955a
Fokusnik909 <Fokusnik90999@gmail.com>
2024-01-04 16:58:08 +0500

    completed 6 sprint

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 04.01.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 04.01.2024.
+//
+
+import Foundation
