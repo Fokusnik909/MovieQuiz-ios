7fad5c7f3ee5df212e75e2782c6b3cca9816ded8
Fokusnik909 <Fokusnik90999@gmail.com>
2024-01-18 00:38:00 +0500

    corrected the comments

---
+++ MovieQuiz/Services/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  MovieQuiz
+//
+//  Created by Артур  Арсланов on 17.01.2024.
+//
+
+import Foundation
---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Артур  Арсланов on 07.01.2024.
+//
+
+import Foundation
